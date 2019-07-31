56bef7e0ed3ef023fda8731952d42da7ed2b01ea
Yumi <ymym2103@gmail.com>
2019-07-31 10:38:27 +0800

    Pokemon

--- pokemon.php
+++ pokemon.php
@@ -1,10 +1,35 @@
+<?php
 class Pokemon
 {
     public $level;
+    private $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
 
     public function attack()
     {
         echo '攻撃';
-        
+
     }
+     public function getName()
+     {
+         return $this->name;
+     }
 }
+$pokemon = new Pokemon('ピカチュウ');
+
+$pokemon->level = 3;
+echo $pokemon->level;
+$pokemon->attack();
+
+//echo $pokemon->name;
+
+$fushigidane = new Pokemon('フシギダネ');
+$zenigame = new Pokemon('ゼニガメ');
+
+echo $fushigidane->getName();
+echo $zenigame->getName();
+
